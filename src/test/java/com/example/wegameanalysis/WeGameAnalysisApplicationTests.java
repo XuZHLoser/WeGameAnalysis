2d080ee52d64e259e86bb2765e25230baede1f52
maohuihua <huihuamao@whut.edu.cn>
2022-03-07 21:52:47 +0800

    First commit: 1.create the skeleton

---
+++ src/test/java/com/example/wegameanalysis/WeGameAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wegameanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeGameAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
